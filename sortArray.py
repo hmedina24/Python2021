36618b2f55c9572f481df807fee204413855f670
Hector Medina <47004857+hmedina24@users.noreply.github.com>
2021-01-03 17:17:40 -0500

    Create sortArray.py

---
+++ sortArray.py
@@ -0,0 +1,12 @@
+def main():
+    #lets create an array
+    nums = [9,3,6,2,7,1,5,4]
+    
+    #built in sorting algorithm
+    """offers the ability to sort data out of the box using sorted() For example: sorted(array) can sort any list as long as the values are comparable"""
+    #print(sorted(nums))
+
+if __name__ == "__main__":
+    main()
+
+ 
